efbb5bc97904a13b7d82f5facb5269d75e624799
Ashish Sasatte <ashishsasatte@gmail.com>
2021-06-12 21:54:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Ashish Sasatte Banking System
+Sparks Foundation Internship Project : Basic Banking System  
+A Web Application used to transfer money between multiple users.  
+
+Front-end : HTML, CSS, Bootstrap
+Back-end : PHP 
+Database : MySQL   
+
+Database contains two Tables- Users Table & Transaction Table 
+1. User table have basic fields such as name, email & current balance. 
+2. Transaction table records all transfers happened along with their time.  
+
+Flow of the Website: Home Page > View all Users > Select and View one User > Transfer Money > Select reciever > View all Users > View Transfer History.
+
+Thank You
---
+++ navbar.php
@@ -0,0 +1,22 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php" style="color : #001133;"><b>Asneh Bank Of India</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : #001133;"><b>HOME</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color : #001133;"><b>CREATE USER</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : #001133;"><b>TRANSFER MONEY</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : #001133;"><b>TRANSACTION HISTORY</b></a>
+              </li>
+          </div>
+       </nav>
